ccc9f9432db0eceb99de1bdb1cd26e13ce84a6c8
Victor Guerra <vguerra@gmail.com>
2017-10-13 03:04:02 +0200

    [WIP] - Steering control
    Introducing steering control using a PID controller and Yaw controller.
    PID controller gives us the corrective part of the steering, Yaw controller
    the predictive one.
    
    For the steering PID controller we need to know the car's CTE, therefore
    we need to suscribe to /final_waypoints and /current_pose.

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,14 +1,63 @@
+import rospy
+
+from lowpass import LowPassFilter
+from pid import PID
+from yaw_controller import YawController
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
+PRED_STEERING_FACTOR = 1.0
+
 
 class Controller(object):
-    def __init__(self, *args, **kwargs):
-        # TODO: Implement
-        pass
+    def __init__(self,
+                 vehicle_mass,
+                 fuel_capacity,
+                 brake_deadband,
+                 decel_limit,
+                 accel_limit,
+                 wheel_radius,
+                 wheel_base,  # YawController
+                 steer_ratio,  # YawController
+                 max_lat_accel,  # YawController
+                 max_steer_angle):  # YawController
 
-    def control(self, *args, **kwargs):
-        # TODO: Change the arg, kwarg list to suit your needs
-        # Return throttle, brake, steer
-        return 1., 0., 0.
+        self.prev_time = rospy.get_time()
+
+        self.steer_pid = PID(kp=0.2, ki=0.0009, kd=1.5, mn=-max_steer_angle, mx=max_steer_angle)
+
+        self.yaw_controller = YawController(wheel_base=wheel_base,
+                                            steer_ratio=steer_ratio,
+                                            min_speed=2.0,
+                                            max_lat_accel=max_lat_accel,
+                                            max_steer_angle=max_steer_angle)
+
+    def control(self,
+                dbw_enabled,
+                cte,
+                linear_velocity,
+                angular_velocity,
+                current_velocity):
+
+        throttle = 0.3
+        brake = 0.0
+        steer = 0.0
+        steering = 0.0
+
+        if dbw_enabled:
+            current_time = rospy.get_time()
+            sample_time = current_time - self.prev_time
+            self.prev_time = current_time
+
+            predictive_steering = self.yaw_controller.get_steering(linear_velocity=linear_velocity,
+                                                        angular_velocity=angular_velocity,
+                                                        current_velocity=current_velocity)
+            corrective_steering = self.steer_pid.step(cte, sample_time)
+
+            rospy.logwarn('steer = %f, steering = %f, cte = %f, sample_time = %f', steer, steering, cte, sample_time)
+        else:
+            self.steer_pid.reset()
+            self.prev_time = rospy.get_time()
+
+        return throttle, brake, steer + PRED_STEERING_FACTOR * steering
